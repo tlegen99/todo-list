51f21f3de1f01a03ad1a0b924dccc2f16ca06a5e
ta.sinergium.ru <ta@sinergium.ru>
2023-05-26 00:54:25 +0500

    удаляет лишнее, добавляет параметр в пдо кодировку

--- components/Db.php
+++ components/Db.php
@@ -9,7 +9,7 @@ class Db
         $paramsPath = ROOT . '/config/db_params.php';
         $params     = include($paramsPath);
         
-        $dns = "mysql:host={$params['host']};dbname={$params['dbname']}";
+        $dns = "mysql:host={$params['host']};dbname={$params['dbname']};charset=utf8";
         $db  = new \PDO($dns, $params['user'], $params['password']);
         
         return $db;
